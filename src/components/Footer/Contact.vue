b2624ee5102de89d9629e8830222281e8951bcc8
javivi235 <javisoruco235@gmail.com>
2019-10-25 15:34:28 -0400

    RF3: footer grafico agregado

---
+++ src/components/Footer/Contact.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-content>
+        <v-card dark>
+            <v-card-title>Contacto</v-card-title>
+            <v-list-item two-line>
+
+                <v-list-item-content>
+                    <v-list-item-title class="headline">Email</v-list-item-title>
+                    <v-list-item-subtitle>info@museodorbigny.org</v-list-item-subtitle>
+                </v-list-item-content>
+
+            </v-list-item>
+
+            <v-list-item two-line>
+
+                <v-list-item-content>
+                    <v-list-item-title class="headline">Teléfono</v-list-item-title>
+                    <v-list-item-subtitle>(+591 4) 44 8 6969</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item four-line>
+
+                <v-list-item-content>
+                    <v-list-item-title class="headline">Dirección</v-list-item-title>
+                    <v-list-item-subtitle>Museo de Historia Natural Alcide d'Orbigny</v-list-item-subtitle>
+                    <v-list-item-subtitle>Av. Potosi Nº 1458</v-list-item-subtitle>
+                    <v-list-item-subtitle>Cochabamba - Bolivia</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+        </v-card>
+    </v-content>
+</template>
+<script>
+export default {
+  name: 'Contact'
+}
+</script>
+<style scoped>
+
+</style>

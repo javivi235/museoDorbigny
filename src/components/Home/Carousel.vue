05dc7cb343014904f8e415752f9137ac98f9153a
jxuxaxcxo <joaquin.vargaselio@gmail.com>
2019-10-28 02:54:38 -0400

    RFE-1: Carousel Added

---
+++ src/components/Home/Carousel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div >
+      <v-carousel id='carousel'>
+            <v-carousel-item  src="@/assets/test1.jpg"/>
+            <v-carousel-item  src="@/assets/test2.jpg"/>
+
+      </v-carousel>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+
+#carousel{
+    padding-top: 10vh;
+    height: 100vh !important;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="home">
+    <Carousel/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import Carousel from '../components/Home/Carousel'
+
 export default {
   name: 'home',
   components: {
+    Carousel
   }
 }
 </script>

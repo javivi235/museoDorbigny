0036c46ce8bc162fd57ec53cee2615b719f6f356
Ángel Z <angel4441916@gmail.com>
2019-11-10 21:25:44 -0400

    footer modificable agregado

--- src/components/Footer/Contact.vue
+++ src/components/Footer/Contact.vue
@@ -6,7 +6,7 @@
 
                 <v-list-item-content>
                     <v-list-item-title class="headline">Email</v-list-item-title>
-                    <v-list-item-subtitle id="f-email">info@museodorbigny.org</v-list-item-subtitle>
+                    <v-list-item-subtitle id="f-email">{{ email }}</v-list-item-subtitle>
                 </v-list-item-content>
 
             </v-list-item>
@@ -15,7 +15,7 @@
 
                 <v-list-item-content>
                     <v-list-item-title id="f-phone" class="headline">Teléfono</v-list-item-title>
-                    <v-list-item-subtitle id="f-number">(+591 4) 44 8 6969</v-list-item-subtitle>
+                    <v-list-item-subtitle id="f-number">{{ phone }}</v-list-item-subtitle>
                 </v-list-item-content>
             </v-list-item>
 
@@ -23,17 +23,43 @@
 
                 <v-list-item-content>
                     <v-list-item-title id="f-direction" class="headline">Dirección</v-list-item-title>
-                    <v-list-item-subtitle id="f-name">Museo de Historia Natural Alcide d'Orbigny</v-list-item-subtitle>
-                    <v-list-item-subtitle id="f-adress">Av. Potosi Nº 1458</v-list-item-subtitle>
-                    <v-list-item-subtitle id="f-city">Cochabamba - Bolivia</v-list-item-subtitle>
+                    <v-list-item-subtitle id="f-name">{{ name }}</v-list-item-subtitle>
+                    <v-list-item-subtitle id="f-adress">{{ address }}</v-list-item-subtitle>
+                    <v-list-item-subtitle id="f-city">{{ city }}</v-list-item-subtitle>
                 </v-list-item-content>
             </v-list-item>
         </v-card>
     </v-content>
 </template>
 <script>
+import axios from 'axios'
+
+import { backendURL } from '@/data.js'
+
 export default {
-  name: 'Contact'
+  name: 'Contact',
+  data: function () {
+    return {
+      name: '',
+      phone: '',
+      email: '',
+      address: '',
+      city: ''
+    }
+  },
+  mounted: function () {
+    axios
+      .get(backendURL + '/footers')
+      .then(response => {
+        let contactInfo = response.data[0]
+
+        this.name = contactInfo.nombre
+        this.phone = contactInfo.telefono
+        this.email = contactInfo.email
+        this.address = contactInfo.direccion
+        this.city = contactInfo.city
+      })
+  }
 }
 </script>
 <style scoped>
